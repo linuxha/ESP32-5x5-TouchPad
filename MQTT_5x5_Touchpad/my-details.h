724e0ee3e2fc146e9c6e6d8dfa79adc3fe5d18b4
Neil Cherry <linuxha@linuxha.com>
2022-02-04 09:28:22 -0500

    Renamed the my-dtail.h file to mydefaults.h file and corrected the defines

---
+++ MQTT_5x5_Touchpad/my-details.h
@@ -0,0 +1,12 @@
+#if !defined(MY_DETAILS_H)
+#define MY_DETAILS_H
+
+#error "Comment me out and edit your my-details.h file"
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid        = "REPLACE_WITH_YOUR_SSID";
+const char* password    = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Add your MQTT Broker IP address, example:
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+#endif
